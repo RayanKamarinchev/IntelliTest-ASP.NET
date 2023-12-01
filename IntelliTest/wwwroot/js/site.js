5c07d7914d776f8b5ece52e7575b3a4dcd82eec1
RayanKamarinchev <rayan2008bg@gmail.com>
2023-12-01 14:42:55 +0200

    question textarea expand fixed

--- IntelliTest/wwwroot/js/site.js
+++ IntelliTest/wwwroot/js/site.js
@@ -6,6 +6,6 @@
 [...document.querySelectorAll("div > div.questTextBox.questAnswer > div > textarea")].forEach(e => {
     textAreaAdjust(e)
 });
-[...document.querySelectorAll("#questions > div > div.customRow > div > textarea")].forEach(e => {
+[...document.querySelectorAll("#questions > div > div > div.customRow > div.questTextBox > textarea")].forEach(e => {
     textAreaAdjust(e)
 });
